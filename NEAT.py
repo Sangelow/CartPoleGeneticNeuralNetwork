2a1b1ceddca79c8403ea991f824273995e5d804b
Sangelow <loiseauflavien@orange.fr>
2018-08-06 15:34:08 +0000

    Added compatibility distance calculator.

--- NEAT.py
+++ NEAT.py
@@ -3,6 +3,78 @@ import copy
 from graphviz import Digraph
 import uuid
 
+def compatibility_distance(genome_1,genome_2,c1,c2,c3):
+    E = count_excess_genes(genome_1,genome_2)
+    D = count_disjoint_genes(genome_1,genome_2)
+    N = count_larger_gene_number(genome_1,genome_2)
+    W = calculate_average_weight_matching_genes(genome_1,genome_2)
+    return c1*E/N + c2*D/N + c3*W
+
+def calculate_average_weight_matching_genes(genome_1,genome_2):
+    weight_differences_sum = 0
+    matching_number = 0
+    for innovation_number in genome_1.connection_genes.keys():
+        if innovation_number in list(genome_2.connection_genes.keys()):
+            weight_differences_sum += abs(genome_1.connection_genes[innovation_number].weight - genome_2.connection_genes[innovation_number].weight)
+            matching_number += 1
+    return weight_differences_sum /matching_number
+
+def count_matching_genes(genome_1,genome_2):
+    matching_genes_number = 0
+    # Count the number of matching nodes
+    for innovation_number in genome_1.node_genes.keys():
+        if innovation_number in list(genome_2.node_genes.keys()):
+            matching_genes_number +=1
+    # Count the number of mathcing connections
+    for innovation_number in genome_1.connection_genes.keys():
+        if innovation_number in list(genome_2.connection_genes.keys()):
+            matching_genes_number +=1
+    return matching_genes_number
+
+def count_disjoint_genes(genome_1,genome_2):
+    disjoint_genes_number = 0
+    max_innovation_number_1 = max(genome_1.connection_genes.keys())
+    max_innovation_number_2 = max(genome_2.connection_genes.keys())
+    smallest_max_innovation_number = min(max_innovation_number_1,max_innovation_number_2)
+    for innovation_number in range(1,smallest_max_innovation_number+1):
+        # Count the number of disjoint nodes
+        if innovation_number in list(genome_1.node_genes.keys()) and not(innovation_number in list(genome_2.node_genes.keys())):
+            disjoint_genes_number += 1
+        if not(innovation_number in list(genome_1.node_genes.keys())) and innovation_number in list(genome_2.node_genes.keys()):
+            disjoint_genes_number += 1
+        # Count the number of disjoint connection
+        if innovation_number in list(genome_1.connection_genes.keys()) and not(innovation_number in list(genome_2.connection_genes.keys())):
+            disjoint_genes_number += 1
+        if not(innovation_number in list(genome_1.connection_genes.keys())) and innovation_number in list(genome_2.connection_genes.keys()):
+            disjoint_genes_number += 1
+    return disjoint_genes_number
+
+def count_excess_genes(genome_1,genome_2):
+    excess_genes_number = 0
+    max_innovation_number_1 = max(genome_1.connection_genes.keys())
+    max_innovation_number_2 = max(genome_2.connection_genes.keys())
+    smallest_max_innovation_number = min(max_innovation_number_1,max_innovation_number_2)
+    greatest_max_innovation_number = max(max_innovation_number_1,max_innovation_number_2)
+    for innovation_number in range(1,greatest_max_innovation_number+1):
+        # Count the number of excess nodes
+        if innovation_number in list(genome_1.node_genes.keys()) and not(innovation_number in list(genome_2.node_genes.keys()))  and innovation_number > smallest_max_innovation_number:
+            excess_genes_number += 1
+        if not(innovation_number in list(genome_1.node_genes.keys())) and innovation_number in list(genome_2.node_genes.keys())  and innovation_number > smallest_max_innovation_number:
+            excess_genes_number += 1
+        # Count the number of excess connection
+        if innovation_number in list(genome_1.connection_genes.keys()) and not(innovation_number in list(genome_2.connection_genes.keys()))  and innovation_number > smallest_max_innovation_number:
+            excess_genes_number += 1
+        if not(innovation_number in list(genome_1.connection_genes.keys())) and innovation_number in list(genome_2.connection_genes.keys())  and innovation_number > smallest_max_innovation_number:
+            excess_genes_number += 1
+    return excess_genes_number
+
+def count_larger_gene_number(genome_1,genome_2):
+    gene_number_1 = len(genome_1.node_genes) + len(genome_1.connection_genes)
+    gene_number_2 = len(genome_2.node_genes) + len(genome_2.connection_genes)
+    return max(gene_number_1,gene_number_2)
+
+
+
 class Genome():
 
     def __init__(self, connection_genes, node_genes):
@@ -16,7 +88,7 @@ class Genome():
         return copy.deepcopy(self)
 
     def add_node_gene(self,node):
-        self.node_genes[node.id] = node
+        self.node_genes[node.innovation_number] = node
 
     def add_connection_gene(self,connection):
         self.connection_genes[connection.innovation_number] = connection
@@ -32,14 +104,14 @@ class Genome():
             node_1 = random.choice(list(self.node_genes.values()))
             node_2 = random.choice(list(self.node_genes.values()))
             # Check if the connection is between a node and himself
-            if node_1.id == node_2.id:
+            if node_1.innovation_number == node_2.innovation_number:
                 connection_valid = False
             # Check if the nodes are both INPUT or both OUTPUT
             if (node_1.type=="INPUT" and node_2.type=="INPUT") or (node_1.type=="OUTPUT" and node_2.type=="OUTPUT"):
                 connection_valid = False
             # Check if the connection already exists
             for connection in self.connection_genes.values():
-                if (connection.in_node==node_1.id and connection.out_node==node_2.id) or (connection.in_node==node_2.id and connection.out_node==node_1.id):
+                if (connection.in_node==node_1.innovation_number and connection.out_node==node_2.innovation_number) or (connection.in_node==node_2.innovation_number and connection.out_node==node_1.innovation_number):
                     connection_valid = False
 
         # Find the correct direction of the connection
@@ -49,7 +121,7 @@ class Genome():
         # Generate a random weight
         weight = random.gauss(0,0.3)
         # Create the new connection
-        new_connection = ConnectionGene(node_1.id,node_2.id,weight, True)
+        new_connection = ConnectionGene(node_1.innovation_number,node_2.innovation_number,weight, True)
         # Add the connection to the list
         self.add_connection_gene(new_connection)
 
@@ -59,12 +131,12 @@ class Genome():
         # Deactivate the old connection
         old_connection.disable()
         # Create the new node
-        new_node = NodeGene("HIDDEN",len(self.node_genes)+1) # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< ID MIGHT NOT BE RIGHT
+        new_node = NodeGene("HIDDEN")
         self.add_node_gene(new_node)
         # Create the two new connections
 
-        new_connection_1 = ConnectionGene(old_connection.in_node, new_node.id, 1, True)
-        new_connection_2 = ConnectionGene(new_node.id,old_connection.out_node, old_connection.weight, True)
+        new_connection_1 = ConnectionGene(old_connection.in_node, new_node.innovation_number, 1, True)
+        new_connection_2 = ConnectionGene(new_node.innovation_number,old_connection.out_node, old_connection.weight, True)
         # Add the two connection
         self.add_connection_gene(new_connection_1)
         self.add_connection_gene(new_connection_2)
@@ -105,7 +177,7 @@ class Genome():
                 cluster.node_attr.update(color='white')
                 for node in self.node_genes.values():
                     if node.type==layer_type:
-                        cluster.node(str(node.id))
+                        cluster.node(str(node.innovation_number))
                 cluster.attr(label=layer_type)
 
         with graph.subgraph(name='cluster_hidden') as cluster_hidden:
@@ -113,7 +185,7 @@ class Genome():
             cluster_hidden.attr(color='grey')
             for node in self.node_genes.values():
                 if node.type=="HIDDEN":
-                    cluster_hidden.node(str(node.id))
+                    cluster_hidden.node(str(node.innovation_number))
             cluster_hidden.attr(label='HIDDEN')
 
         for connection in self.connection_genes.values():
@@ -157,9 +229,10 @@ class ConnectionGene():
 
 class NodeGene():
 
-    def __init__(self, type=str(), id=int()):
+    def __init__(self, type=str()):
+        global historical_marker
         self.type = type # The type should be either "INPUT"/"HIDDEN"/"OUTPUT"
-        self.id = id
+        self.innovation_number = historical_marker.get_innovation_number(self)
 
     def copy(self):
         return copy.deepcopy(self)
@@ -168,15 +241,22 @@ class HistoricalMarker():
 
     def __init__(self):
         self.existing_connections = {}
+        self.existing_nodes = {}
         self.innovation_number = 0
 
-    def get_innovation_number(self,new_connection):
-        for connection in self.existing_connections.values():
-            if connection.in_node==new_connection.in_node and connection.out_node==new_connection.out_node:
-                return connection.innovation_number
-        self.innovation_number += 1
-        self.existing_connections[self.innovation_number] = new_connection
-        return self.innovation_number
+    def get_innovation_number(self,new_gene):
+        if new_gene.__class__.__name__ == "ConnectionGene":
+            new_connection = new_gene
+            for connection in self.existing_connections.values():
+                if connection.in_node==new_connection.in_node and connection.out_node==new_connection.out_node:
+                    return connection.innovation_number
+            self.innovation_number += 1
+            self.existing_connections[self.innovation_number] = new_connection
+            return self.innovation_number
+        elif new_gene.__class__.__name__ == "NodeGene":
+            self.innovation_number += 1
+            self.existing_nodes[self.innovation_number] = new_gene
+            return self.innovation_number
 
 if __name__ == "__main__":
 
@@ -188,25 +268,20 @@ if __name__ == "__main__":
 
     genome_1 = Genome({},{})
     for i in range(1,4):
-        genome_1.add_node_gene(NodeGene("INPUT",i))
-    genome_1.add_node_gene(NodeGene("OUTPUT",4))
-    genome_1.add_node_gene(NodeGene("HIDDEN",5))
+        genome_1.add_node_gene(NodeGene("INPUT"))
+    genome_1.add_node_gene(NodeGene("OUTPUT"))
+    genome_1.add_node_gene(NodeGene("HIDDEN"))
+
+    genome_2 = genome_1.copy()
+    genome_2.add_node_gene(NodeGene("HIDDEN"))
 
     genome_1.add_connection_gene(ConnectionGene(1,4,1,True))
     genome_1.add_connection_gene(ConnectionGene(2,4,1,False))
     genome_1.add_connection_gene(ConnectionGene(3,4,1,True))
     genome_1.add_connection_gene(ConnectionGene(2,5,1,True))
     genome_1.add_connection_gene(ConnectionGene(5,4,1,True))
-    genome_1.add_connection_gene(ConnectionGene(1,5,1,True))
 
-    print(genome_1)
-
-    genome_2 = Genome({},{})
-    for i in range(1,4):
-        genome_2.add_node_gene(NodeGene("INPUT",i))
-    genome_2.add_node_gene(NodeGene("OUTPUT",4))
-    genome_2.add_node_gene(NodeGene("HIDDEN",5))
-    genome_2.add_node_gene(NodeGene("HIDDEN",6))
+    
 
     genome_2.add_connection_gene(ConnectionGene(1,4,1,True))
     genome_2.add_connection_gene(ConnectionGene(2,4,1,False))
@@ -215,12 +290,19 @@ if __name__ == "__main__":
     genome_2.add_connection_gene(ConnectionGene(5,4,1,False))
     genome_2.add_connection_gene(ConnectionGene(5,6,1,True))
     genome_2.add_connection_gene(ConnectionGene(6,4,1,True))
+
+    genome_1.add_connection_gene(ConnectionGene(1,5,1,True))
+
     genome_2.add_connection_gene(ConnectionGene(3,5,1,True))
     genome_2.add_connection_gene(ConnectionGene(1,6,1,True))
 
-    genome_1.print_genome()
-    genome_2.print_genome()
+    # genome_1.print_genome()
+    # genome_2.print_genome()
 
+    print("Number of matching gennes : {}.".format(count_matching_genes(genome_1,genome_2)))
+    print("Number of disjoint genes : {}.".format(count_disjoint_genes(genome_1,genome_2)))
+    print("Number of disjoint genes : {}.".format(count_excess_genes(genome_1,genome_2)))
+    print(compatibility_distance(genome_1,genome_2,1,1,1))
     # genome_3 = Genome.crossover(genome_2,genome_1)
 
     # genome_1.add_node_gene_mutatio()
@@ -231,5 +313,3 @@ if __name__ == "__main__":
 
     # genome_1.weight_mutation()
     # genome_1.print_genome()
-
-
