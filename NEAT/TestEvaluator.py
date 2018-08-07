263f95aa9ddb96edb119c213132ff4ddd0b0dc65
Sangelow <loiseauflavien@orange.fr>
2018-08-07 14:09:48 +0000

    Fixed some bugs and reorganized the files.

---
+++ NEAT/TestEvaluator.py
@@ -0,0 +1,26 @@
+from NEAT import Evaluator
+
+class TestEvaluator(Evaluator):
+
+    def evaluate_genome(self,genome):
+        return len(genome.node_genes)
+
+
+if __name__ == "__main__":
+
+    generation_size = 50
+    input_size = 2
+    output_size = 2
+    
+    mutation_rate = 0.8
+    perturbating_probability = 0.9
+    add_connection_mutation_chance = 0.1
+    add_node_mutation_chance = 0.1
+
+    compatibility_distance_threshold = 10
+    c1, c2, c3 = 1, 1, 0.4 
+
+
+    evaluator = TestEvaluator(generation_size, input_size, output_size, mutation_rate, perturbating_probability, add_connection_mutation_chance, add_node_mutation_chance, compatibility_distance_threshold, c1, c2, c3)
+    for i  in range(10):
+        evaluator.evaluate()
